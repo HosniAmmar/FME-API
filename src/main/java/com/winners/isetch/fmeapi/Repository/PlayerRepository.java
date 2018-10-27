ead19106ef001f596f9ce9d23f35afaea9287dc6
FirasBc <firas.bouchami@gmail.com>
2018-10-28 00:28:32 +0100

    Delete PlayerRepository.java

--- src/main/java/com/winners/isetch/fmeapi/Repository/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.winners.isetch.fmeapi.Repository;
-
-import com.winners.isetch.fmeapi.Entity.Player;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayerRepository extends CrudRepository<Player,Integer> {
-
-}
