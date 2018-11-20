f892bdeb3288a5f1268b7fc6f1567c1b1cbbdf5a
Logdone <Firas.bouchami@gmail.com>
2018-11-20 18:39:14 +0100

    //new attributes ready

--- src/main/java/com/winners/isetch/fmeapi/Entity/Responsable.java
+++ src/main/java/com/winners/isetch/fmeapi/Entity/Responsable.java
@@ -22,8 +22,37 @@ public class Responsable {
 	 	private String fonction;
 	 	private String nationality;
 	 	private String photoUrl;
-	 	
+
+	 	//new att
+	 	private String region;
+	 	private String city;
+	 	private String numpasseport;
+	 	private String comment;
+
 	 	public Responsable() {}
+
+	 	
+		public Responsable(int id, String firstName, String lastName, String natIdNum, String adress, String birthdate,
+				String phone, String mailAdress, String fonction, String nationality, String photoUrl, String region,
+				String city, String numpasseport, String comment) {
+			super();
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.natIdNum = natIdNum;
+			this.adress = adress;
+			this.birthdate = birthdate;
+			this.phone = phone;
+			this.mailAdress = mailAdress;
+			this.fonction = fonction;
+			this.nationality = nationality;
+			this.photoUrl = photoUrl;
+			this.region = region;
+			this.city = city;
+			this.numpasseport = numpasseport;
+			this.comment = comment;
+		}
+
 
 		public Responsable(int id, String firstName, String lastName, String natIdNum, String adress, String birthdate,
 				String phone, String mailAdress, String fonction, String nationality, String photoUrl) {
@@ -131,14 +160,58 @@ public class Responsable {
 			this.photoUrl = photoUrl;
 		}
 
+
+		public String getRegion() {
+			return region;
+		}
+
+
+		public void setRegion(String region) {
+			this.region = region;
+		}
+
+
+		public String getCity() {
+			return city;
+		}
+
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+
+		public String getNumpasseport() {
+			return numpasseport;
+		}
+
+
+		public void setNumpasseport(String numpasseport) {
+			this.numpasseport = numpasseport;
+		}
+
+
+		public String getComment() {
+			return comment;
+		}
+
+
+		public void setComment(String comment) {
+			this.comment = comment;
+		}
+
+
 		@Override
 		public String toString() {
 			return "Responsable [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", natIdNum="
 					+ natIdNum + ", adress=" + adress + ", birthdate=" + birthdate + ", phone=" + phone
 					+ ", mailAdress=" + mailAdress + ", fonction=" + fonction + ", nationality=" + nationality
-					+ ", photoUrl=" + photoUrl + "]";
+					+ ", photoUrl=" + photoUrl + ", region=" + region + ", city=" + city + ", numpasseport="
+					+ numpasseport + ", comment=" + comment + "]";
 		}
-	 	
+		
+
+		
 	 	
 	 	 
 
